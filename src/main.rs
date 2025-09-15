0e7980438d6e75945b2fcdedea0d68a2945d4e95
Ansh Kumar <ansh0kumar07@gmail.com>
2025-09-15 21:23:57 +0530

    Basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const GAMES: &str = include_str!("../answers.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let w = roget::Wordle::new();
+    for answer in GAMES.split_whitespace() {
+        let guesser = roget::algorithms::Naive::new();
+        w.play(answer, guesser);
+    }
 }
